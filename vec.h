2414e17efe1f3a0093c295c9251b27576f300723
Pavel F <mihail.karpov196@gmail.com>
2022-08-30 19:34:07 +0300

    *add cross product *add cross product length *rename func TheoremPythagoras to EuclideanDistance *add member - function LengthVec for vec

--- vec.h
+++ vec.h
@@ -6,9 +6,17 @@ constexpr auto PI = 3.141592654;
 
 namespace vec{
 
+//theorem pythagoras 
+template<typename T, size_t N>
+float EuclideanDistance(const std::array<T, N>& args) {
+	float temp(0);
+	for (auto it : args) {
+		temp = temp + (it * it);
+	}
+	return sqrt(temp);
+}
 
-
-//i recomended used float or double for vec2<T>
+//recomended used float or double for vec<T>
 template<typename T, size_t N>
 struct vec {
 	std::array<T, N> data;
@@ -28,8 +36,11 @@ struct vec {
 		}
 		return temp;
 	}
-};
 
+	float LengthVec() const{
+		return EuclideanDistance(data);
+	}
+};
 
 //vector summ
 template<typename T, size_t N>
@@ -59,42 +70,30 @@ std::ostream& operator<<(std::ostream& os, const vec<T,N>& vec) {
 	return os;
 }
 
-
 //product vector & scalar
 template<typename T, typename K, size_t N>
-vec<T,N> operator*(const vec<T,N> & vec,const K& scalar) {
+vec<T,N> operator*(const vec<T,N> & value,const K& scalar) {
 	vec<T, N> temp;
 	for (size_t i = 0; i < N; i++) {
-		temp.data[i] = lhs.data[i] * rhs.data[i];
+		temp.data[i] = value.data[i] * value.data[i];
 	}
 	return temp;
 }
 
 template<typename T, size_t N>
-vec<T,N> abs(const vec<T,N>& vec) {
+vec<T,N> abs(const vec<T,N>& value) {
 	vec<T, N> temp;
 	for (size_t i = 0; i < N; i++) {
-		temp.data[i] = abs(vec.data[i]);
+		temp.data[i] = abs(value.data[i]);
 	}
 	return temp;
-}
-
-//theorem pythagoras 
-template<typename T, size_t N>
-float TheoremPythagoras(std::array<T,N> args) {
-	float temp (0);
-	for (auto it : vec.data) {
-		temp = temp + (it * it);
-	}
-	return sqrt(temp);
 }
 
 //length vector
 template<typename T, size_t N>
-float LengthVec(const vec<T,N>& vec) {
-	return TheoremPythagoras(vec.data);
+float LengthVec(const vec<T,N>& value) {
+	return EuclideanDistance(value.data);
 }
-
 
 //vector distance
 template<typename T, size_t N>
@@ -129,9 +128,25 @@ float Angle(const vec<T,N>& lhs, const vec<T,N>& rhs) {
 	return acos((Dot(lhs, rhs)) / (LengthVec(lhs) * LengthVec(rhs)));
 }
 
+//scalar product for not Euclid space
 template<typename T, size_t N>
 float MathDot(const vec<T,N>& lhs, const vec<T,N>& rhs) {
-	return LengthVec(lhs) * LengthVec(rhs) * cos(Angle(lhs, rhs));z
+	return LengthVec(lhs) * LengthVec(rhs) * cos(Angle(lhs, rhs));
+}
+
+//cross product
+template<typename T>
+vec<T, 3> CrossProduct(const vec<T, 3>& lhs, const vec<T, 3>& rhs) {
+	vec<T, 3> temp = { 0,0,0 };
+	temp.data[0] = (lhs.data[1] * rhs.data[2]) - (lhs.data[2] * rhs.data[1]);
+	temp.data[1] = -((lhs.data[0] * rhs.data[2]) - (lhs.data[2] * rhs.data[0]));
+	temp.data[2] = (lhs.data[0] * rhs.data[1]) - (lhs.data[1] * rhs.data[0]);
+	return temp;
+}
+
+template<typename T, size_t N>
+float CrossLength(const vec<T, N>& lhs, const vec<T, N>& rhs) {
+	return (lhs.LengthVec() * rhs.LengthVec()) * sin(Angle(lhs, rhs));
 }
 
 }//end namespace vec
